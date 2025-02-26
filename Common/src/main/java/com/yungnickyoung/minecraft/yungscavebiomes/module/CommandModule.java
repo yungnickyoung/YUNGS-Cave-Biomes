55c47690de4d3bef0dc9abdb2e60105e02bd5cc5
Nick Young <yungnickyoung@gmail.com>
2025-02-25 21:57:24 -0500

    Make debug commands only register in dev environment

--- Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/module/CommandModule.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/module/CommandModule.java
@@ -5,6 +5,7 @@ import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegisterCommand
 import com.yungnickyoung.minecraft.yungscavebiomes.YungsCaveBiomesCommon;
 import com.yungnickyoung.minecraft.yungscavebiomes.command.DebugSpawnArmorTrimsCommand;
 import com.yungnickyoung.minecraft.yungscavebiomes.command.SandstormCommand;
+import com.yungnickyoung.minecraft.yungscavebiomes.services.Services;
 import net.minecraft.server.commands.SpawnArmorTrimsCommand;
 
 @AutoRegister(YungsCaveBiomesCommon.MOD_ID)
@@ -12,10 +13,18 @@ public class CommandModule {
     @AutoRegister("sandstorm")
     public static final AutoRegisterCommand SANDSTORM_COMMAND = AutoRegisterCommand.of(SandstormCommand::register);
 
+    // DEBUG commands
     @AutoRegister("ycb_armor_trims")
-    public static final AutoRegisterCommand DEBUG_ARMOR_TRIM_COMMAND = AutoRegisterCommand.of(DebugSpawnArmorTrimsCommand::register);
+    public static final AutoRegisterCommand DEBUG_ARMOR_TRIM_COMMAND = AutoRegisterCommand.of((dispatcher, ctx, handler) -> {
+        // Only register in dev environment
+        if (!Services.PLATFORM.isDevelopmentEnvironment()) return;
+        DebugSpawnArmorTrimsCommand.register(dispatcher, ctx, handler);
+    });
 
     @AutoRegister("spawn_armor_trims")
-    public static final AutoRegisterCommand DEBUG_ALL_ARMOR_TRIM_COMMAND = AutoRegisterCommand.of((dispatcher, ctx, handler) ->
-            SpawnArmorTrimsCommand.register(dispatcher));
+    public static final AutoRegisterCommand DEBUG_ALL_ARMOR_TRIM_COMMAND = AutoRegisterCommand.of((dispatcher, ctx, handler) -> {
+        // Only register in dev environment
+        if (!Services.PLATFORM.isDevelopmentEnvironment()) return;
+        SpawnArmorTrimsCommand.register(dispatcher);
+    });
 }
